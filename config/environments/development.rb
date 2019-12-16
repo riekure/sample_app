34e2293913e78236bcf9a60ea7335e8f1570e93a
riekure <s2darl0uvgia24@gmail.com>
2019-12-16 14:54:38 +0000

    リスト11.18まで済

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '59da141d4d1c469d84fef3ed4b976060.vfs.cloud9.us-east-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
