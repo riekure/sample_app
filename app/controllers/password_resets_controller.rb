3e84e0d8a4f09857d8c25fc79159feb50211203b
riekure <s2darl0uvgia24@gmail.com>
2019-12-25 13:59:14 +0000

    テンプレ修正、リスト12.15まで

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -8,7 +11,7 @@ class PasswordResetsController < ApplicationController
       @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
-      redirect_to roo_url
+      redirect_to root_url
     else
       flash[:danger] = "Email address not found"
       render 'new'
@@ -17,4 +20,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+    
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
